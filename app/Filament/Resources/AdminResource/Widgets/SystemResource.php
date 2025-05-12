47522651b142cbc0d13be10413e2f3b118be34a5
Ilham Safatullah <aqsal.ava@gmail.com>
2025-05-12 14:47:04 +0700

    make the monitoring respect config from config/monitoring.php

--- app/Filament/Resources/AdminResource/Widgets/SystemResource.php
+++ app/Filament/Resources/AdminResource/Widgets/SystemResource.php
@@ -6,7 +6,6 @@ use App\Models\Interface\Monitoring;
 use App\Models\System\Resource;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Illuminate\Support\Facades\Concurrency;
 use Illuminate\Support\Number;
 
 class SystemResource extends BaseWidget
@@ -21,34 +20,41 @@ class SystemResource extends BaseWidget
     protected function getStats(): array
     {
         // dd(\App\Helpers\Helper::server_checkup());
-        if (! \App\Helpers\Helper::server_checkup()){
-            return [view('livewire.server-info')];
+        if (!\App\Helpers\Helper::server_checkup()) {
+            return [view("livewire.server-info")];
         }
         $resource = Resource::first();
-        $monitoring = Monitoring::monitor("ether1");
-        $cpu_usage = $resource['cpu-load'];
-        $memory_usg = $resource['total-memory'] - $resource['free-memory'];
-        $traffic_usg = $monitoring[0]['rx-bits-per-second'] / 1024 / 1024;
+        $monitoring = Monitoring::monitor(
+            config("monitoring.monitoring.interface")
+        );
+        $cpu_usage = $resource["cpu-load"];
+        $memory_usg = $resource["total-memory"] - $resource["free-memory"];
+        $traffic_usg = $monitoring[0]["rx-bits-per-second"] / 1024 / 1024;
         $traffic_usg = Number::format($traffic_usg, 2);
         array_push($this->cpu_usage_history, $cpu_usage);
         array_push($this->mem_usage_history, $memory_usg);
         array_push($this->traffic_usage_history, $traffic_usg);
-        $memory_usg_percent = Number::format($memory_usg/$resource['total-memory']*100, 2);
+        $memory_usg_percent = Number::format(
+            ($memory_usg / $resource["total-memory"]) * 100,
+            2
+        );
         return [
             Stat::make("CPU Usage", "$cpu_usage%")
-            ->description("{$resource['cpu']} (x{$resource['cpu-count']}) {$resource['cpu-frequency']} MHz")
-            ->chart($this->cpu_usage_history)
-            // ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('success'),
+                ->description(
+                    "{$resource["cpu"]} (x{$resource["cpu-count"]}) {$resource["cpu-frequency"]} MHz"
+                )
+                ->chart($this->cpu_usage_history)
+                // ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color("success"),
             Stat::make("Memory Usage", "$memory_usg_percent%")
                 ->chart($this->mem_usage_history)
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('info'),
+                ->descriptionIcon("heroicon-m-arrow-trending-up")
+                ->color("info"),
             Stat::make("Traffic Monitor", "$traffic_usg Mbps")
                 ->chart($this->traffic_usage_history)
                 ->color("warning")
                 ->description("Download Traffic of {$monitoring[0]["name"]}")
-                ->descriptionIcon("heroicon-m-arrow-trending-up")
+                ->descriptionIcon("heroicon-m-arrow-trending-up"),
         ];
     }
 }
